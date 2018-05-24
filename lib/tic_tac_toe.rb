47367ef5d1dca10191a02d150dd5dccd1bc89506
popechrisj@gmail.com <popechrisj@gmail.com>
2018-05-24 18:30:58 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -10,7 +10,7 @@ class TicTacToe
       end
     end
     if won?()
-      puts "Congratulations #{winner(@board)}!"
+      puts "Congratulations #{winner()}!"
     elsif draw?()
       puts "Cat's Game!"
     end
