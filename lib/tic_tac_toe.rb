ebd0df39774af205cd9563588ff52df39c1efcd1
popechrisj@gmail.com <popechrisj@gmail.com>
2018-05-24 18:26:07 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,34 +1,34 @@
 class TicTacToe
   def initialize
     @board = Array.new(9, " ")
-  end 
-  def play(@board)
+  end
+  def play()
     until over?(@board)
     turn(@board)
     if draw?(@board)
       break
     end
     end
-    if won?(@board)
+    if won?()
       puts "Congratulations #{winner(@board)}!"
-    elsif draw?(@board)
+    elsif draw?()
       puts "Cat's Game!"
     end
-  end  
-  def turn(@board)
+  end
+  def turn()
     puts "Please enter 1-9:"
     input = gets.chomp
     #input = gets.strip
     index = input_to_index(input)
-    if valid_move?(@board, index)
-      current_player = current_player(@board)
-      move(@board, index, current_player)
-      display_board(@board)
+    if valid_move?(index)
+      current_player = current_player())
+      move(index, current_player)
+      display_board()
     else
-      turn(@board)
+      turn()
     end
   end
-  def display_board(@board)
+  def display_board()
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
     puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
@@ -38,21 +38,21 @@ class TicTacToe
   def input_to_index(user_input)
     user_input.to_i - 1
   end
-  #def move(@board, index, current_player = "X")
-  def move(@board, index, current_player)
+  #def move(board, index, current_player = "X")
+  def move(index, current_player)
     @board[index] = current_player
-    #@board[index] = current_player(@board)
+    #@board[index] = current_player(board)
   end
-  def position_taken?(@board, location)
+  def position_taken?(location)
     @board[location] != " " && @board[location] != ""
   end
-  def valid_move?(@board, index)
-    index.between?(0,8) && !position_taken?(@board, index)
+  def valid_move?(index)
+    index.between?(0,8) && !position_taken?(index)
   end
-  def position_taken?(@board, index)
+  def position_taken?(index)
     !(@board[index].nil? || @board[index] == " " || @board[index] == "")
   end
-  def turn_count(@board)
+  def turn_count()
     turn = 0
     @board.each do |space|
       if space == "X" or space == "O"
@@ -61,8 +61,8 @@ class TicTacToe
     end
     turn
   end
-  def current_player(@board)
-    turn_count(@board) % 2 == 0 ? "X" : "O"
+  def current_player()
+    turn_count() % 2 == 0 ? "X" : "O"
   end
   WIN_COMBINATIONS = [
     [0, 1, 2],
@@ -74,33 +74,33 @@ class TicTacToe
     [0, 4, 8],
     [2, 4, 6]
   ]
-  def won?(@board)
+  def won?()
     WIN_COMBINATIONS.select do |row|
-      if position_taken?(@board, row[0]) && @board[row[0]] == @board[row[1]] && @board[row[1]] == @board[row[2]]
+      if position_taken?(row[0]) && @board[row[0]] == @board[row[1]] && @board[row[1]] == @board[row[2]]
         return row
       end
     end
     return false
   end
-  def full?(@board)
+  def full?()
     WIN_COMBINATIONS.select do |row|
       row.select do |element|
-        if !position_taken?(@board, element)
+        if !position_taken?(element)
           return false
         end
       end
     end
   true
   end
-  def draw?(@board)
-    !won?(@board) && full?(@board)
+  def draw?()
+    !won?() && full?()
   end
-  def over?(@board)
-    won?(@board) || full?(@board)
+  def over?()
+    won?() || full?()
   end
-  def winner(@board)
-    if won?(@board).is_a?(Array)
-      return @board[won?(@board)[0]]
+  def winner()
+    if won?().is_a?(Array)
+      return @board[won?()[0]]
     else
       return nil
     end
